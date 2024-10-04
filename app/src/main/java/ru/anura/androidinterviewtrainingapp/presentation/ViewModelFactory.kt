56a9a8c745d87864b8c1f908c8ea6350a4e2c009
anura1402 <ann99909@gmail.com>
2024-10-04 22:47:26 +0300

    changing factory due to viewModelProviders

--- app/src/main/java/ru/anura/androidinterviewtrainingapp/presentation/ViewModelFactory.kt
+++ app/src/main/java/ru/anura/androidinterviewtrainingapp/presentation/ViewModelFactory.kt
@@ -15,50 +15,52 @@ import ru.anura.androidinterviewtrainingapp.domain.usecases.GetTestWithWrongQUse
 import ru.anura.androidinterviewtrainingapp.domain.usecases.GetTheoryListUseCase
 import ru.anura.androidinterviewtrainingapp.domain.usecases.IsThemePassedUseCase
 import javax.inject.Inject
+import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    //private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ChildQuestionViewModelFactory>>
-    private val changeIsCorrectUseCase: ChangeIsCorrectUseCase,
-    private val changeIsFavUseCase: ChangeIsFavUseCase,
-
-    private val generateTestCurrentThemeUseCase: GenerateTestCurrentThemeUseCase,
-    private val generateTestUseCase: GenerateTestUseCase,
-    private val getTestWithWrongQUseCase: GetTestWithWrongQUseCase,
-    private val getTestWithFavQUseCase: GetTestWithFavQUseCase,
-    private val getCountOfQuestionsUseCase: GetCountOfQuestionsUseCase,
-    private val getCountOfQuestionsByCurrentThemeUseCase: GetCountOfQuestionsByCurrentThemeUseCase,
-
-    private val theme: Theme,
-
-    private val getCorrectAnsweredCountUseCase: GetCorrectAnsweredCountUseCase,
-    private val isThemePassedUseCase: IsThemePassedUseCase,
-    private val getTheoryListUseCase: GetTheoryListUseCase,
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+//    private val changeIsCorrectUseCase: ChangeIsCorrectUseCase,
+//    private val changeIsFavUseCase: ChangeIsFavUseCase,
+//
+//    private val generateTestCurrentThemeUseCase: GenerateTestCurrentThemeUseCase,
+//    private val generateTestUseCase: GenerateTestUseCase,
+//    private val getTestWithWrongQUseCase: GetTestWithWrongQUseCase,
+//    private val getTestWithFavQUseCase: GetTestWithFavQUseCase,
+//    private val getCountOfQuestionsUseCase: GetCountOfQuestionsUseCase,
+//    private val getCountOfQuestionsByCurrentThemeUseCase: GetCountOfQuestionsByCurrentThemeUseCase,
+//
+//    private val theme: Theme,
+//
+//    private val getCorrectAnsweredCountUseCase: GetCorrectAnsweredCountUseCase,
+//    private val isThemePassedUseCase: IsThemePassedUseCase,
+//    private val getTheoryListUseCase: GetTheoryListUseCase,
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass == QuestionViewModel::class.java) {
-            return QuestionViewModel(
-                changeIsCorrectUseCase,
-                changeIsFavUseCase,
-                generateTestCurrentThemeUseCase,
-                generateTestUseCase,
-                getTestWithWrongQUseCase,
-                getTestWithFavQUseCase,
-                getCountOfQuestionsUseCase,
-                getCountOfQuestionsByCurrentThemeUseCase,
-                theme
-            ) as T
-        } else if (modelClass == WelcomeViewModel::class.java) {
-            return WelcomeViewModel(
-                getCountOfQuestionsUseCase,
-                getCorrectAnsweredCountUseCase,
-                isThemePassedUseCase
-            ) as T
-        } else if (modelClass == TheoryListViewModel::class.java) {
-            return TheoryListViewModel(
-                getTheoryListUseCase,
-                theme
-            ) as T
-        }
-        throw RuntimeException("Unknown view model class $modelClass")
+//        if (modelClass == QuestionViewModel::class.java) {
+//            return QuestionViewModel(
+//                changeIsCorrectUseCase,
+//                changeIsFavUseCase,
+//                generateTestCurrentThemeUseCase,
+//                generateTestUseCase,
+//                getTestWithWrongQUseCase,
+//                getTestWithFavQUseCase,
+//                getCountOfQuestionsUseCase,
+//                getCountOfQuestionsByCurrentThemeUseCase,
+//                theme
+//            ) as T
+//        } else if (modelClass == WelcomeViewModel::class.java) {
+//            return WelcomeViewModel(
+//                getCountOfQuestionsUseCase,
+//                getCorrectAnsweredCountUseCase,
+//                isThemePassedUseCase
+//            ) as T
+//        } else if (modelClass == TheoryListViewModel::class.java) {
+//            return TheoryListViewModel(
+//                getTheoryListUseCase,
+//                theme
+//            ) as T
+//        }
+//        throw RuntimeException("Unknown view model class $modelClass")
+        return viewModelProviders[modelClass]?.get() as T
     }
 }
